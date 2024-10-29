5ab6c7c3cb336fb6f0633c9c2116913a40a0e1b2
narapark7561 <narapark7561@gmail.com>
2024-10-28 22:32:51 -0600

    Completed Lab 3 (Props and State Management)

---
+++ Lab3_mobile/components/ToDoList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ScrollView, Text, View, StyleSheet, Pressable } from 'react-native';
+
+export default function ToDoList({ tasks }) {
+  return (
+    <ScrollView>
+      {tasks.map((task, index) => (
+        <Pressable key={index}>
+          <View style={[styles.task]}>
+            <Text style={styles.taskText}>{task}</Text>
+          </View>
+        </Pressable>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
