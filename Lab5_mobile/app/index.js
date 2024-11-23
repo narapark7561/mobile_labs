4d09821f2045f61e98dfcc431bae122da48d6f18
narapark7561 <narapark7561@gmail.com>
2024-11-23 13:56:15 -0700

    Starting Lab 5

---
+++ Lab5_mobile/app/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import App from '../components/app';
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <App />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main: {
+    width: '100%',
+    padding: 20,
+  },
+});
